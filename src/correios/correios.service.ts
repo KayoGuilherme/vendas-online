1a0d8489193f35a45ebf6c8574848400fd36317c
Prizzy12 <ivailsonqueiroz9@gmail.com>
2024-08-18 18:19:38 -0300

    files: tirando arquivos secretos

--- src/correios/correios.service.ts
+++ src/correios/correios.service.ts
@@ -14,7 +14,7 @@ export class CorreiosService {
     cdService: string,
     cep: string,
     sizeProduct: SizeProductDTO,
-  ): Promise<ResponsePriceCorreios> {
+  ) {
     try {
       const response = await axios.post(
         this.API_URL,
@@ -22,8 +22,8 @@ export class CorreiosService {
           idLote: '1234567890',
           parametrosProduto: [
             {
-              coProduto: "04162",
-                nuRequisicao: "12345",
+              coProduto: '04162',
+              nuRequisicao: '12345',
               cdServico: cdService,
               cepOrigem: this.CEP_COMPANY,
               cepDestino: cep,
@@ -49,38 +49,11 @@ export class CorreiosService {
           },
         },
       );
-
-      if (response.data) {
-        return response.data as ResponsePriceCorreios;
-      } else {
-        throw new BadRequestException('No response from API');
-      }
     } catch (error) {
-      if (error.response) {
-        console.error('API Response Error:', {
-          status: error.response.status,
-          data: error.response.data,
-          headers: error.response.headers,
-        });
-
-        if (error.response.status === 503) {
-          throw new BadRequestException(
-            'Serviço indisponível ou endpoint incorreto.',
-          );
-        }
-
-        throw new BadRequestException(
-          `Erro da API: ${error.response.data.msgs.join(', ')}`,
-        );
-      } else if (error.request) {
-        console.error('API Request Error:', error.request);
-        throw new BadRequestException('Não foi recebida resposta da API');
-      } else {
-        console.error('Error Configurando Requisição:', error.message);
-        throw new BadRequestException(
-          `Erro ao fazer a requisição: ${error.message}`,
-        );
-      }
+      console.error('Error Configurando Requisição:', error.message);
+      throw new BadRequestException(
+        `Erro ao fazer a requisição: ${error.message}`,
+      );
     }
   }
 }
